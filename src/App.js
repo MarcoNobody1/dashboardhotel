e8a39aff7d3eeade9881e5ae86c58ffa297d8612
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-09-26 17:00:06 +0200

    Se añade lógica de rutas y de la página de log in, se implementa sistema basico de log con localStorage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./Login/Login";
+import { Dashboard } from "./Dashboard/Dashboard";
+import { PrivateRoute } from "./Login/PrivateRoute";
+import { Bookings } from "./Bookings/Bookings";
+import { Rooms } from "./Rooms/Rooms";
+import { Contact } from "./Contact/Contact";
+import { Users } from "./Users/Users";
 
-import './App.css';
+export const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
 
-function App() {
-  return ;
-}
-
-export default App;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute loggedIn={loggedIn}>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/bookings" element={<Bookings />} />
+          <Route path="/rooms" element={<Rooms />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
