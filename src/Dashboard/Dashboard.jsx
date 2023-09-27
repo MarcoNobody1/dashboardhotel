e88439dbb242a2afb0a4fe626478ec0678ac138a
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-09-27 23:16:51 +0200

    Se incluye apartado de notificaciones en el dashboard(KPIs)

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,12 +1,33 @@
 import React from "react";
-import { PageWrapper } from "../GeneralComponents";
+import { Notification, PageWrapper } from "../GeneralComponents";
+import { LiaBedSolid } from "react-icons/lia";
+import { LuCalendarCheck2 } from "react-icons/lu";
+import { BsBoxArrowInRight, BsBoxArrowRight } from "react-icons/bs";
+import styled from "styled-components";
+
+const NotifContainer = styled.div`
+  display: flex;
+  width: 1474px;
+  margin-bottom: 40px;
+`;
 
 export const Dashboard = () => {
   return (
     <>
-        <PageWrapper>
-          Dashboard
-        </PageWrapper>
+      <PageWrapper>
+        <NotifContainer>
+          <Notification number="8,765" text="New Booking" icon={LiaBedSolid} />
+          <Notification
+            number="963"
+            text="Scheduled Room"
+            icon={LuCalendarCheck2}
+          />
+          <Notification number="753" text="Check In" icon={BsBoxArrowInRight} />
+          <Notification number="516" text="Check Out" icon={BsBoxArrowRight} />
+        </NotifContainer>
+
+
+      </PageWrapper>
     </>
   );
 };
