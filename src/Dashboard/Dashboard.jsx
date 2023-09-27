f6a8b3815c96449be6640dfeeb0f57225a84cdc9
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-09-27 02:22:40 +0200

    Se empieza styling del dashboard

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const OuterContainer = styled.div`
+  background-color: #f8f8f8;
+  width: 100%;
+  height: 100%;
+`;
 
 export const Dashboard = () => {
   return (
     <>
-      <div>Dashboard</div>
+      <OuterContainer>Dashboard</OuterContainer>
     </>
   );
 };
