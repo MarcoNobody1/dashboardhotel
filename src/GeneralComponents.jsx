d0be07c34477863023845f7f37695f3ebbfab726
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-09-27 23:16:01 +0200

    Se crea componente general para las notificaciones del dashboard(KPI)

--- src/GeneralComponents.jsx
+++ src/GeneralComponents.jsx
@@ -12,9 +12,90 @@ export const DefaultIcon = styled.div.attrs((props) => ({
   }
 `;
 
-export const PageWrapper = styled.div`
+export const PageWrapper = styled.main`
   background-color: #f8f8f8;
-  width: 100%;
-  height: 100vh;
-
+  height: 810px;
+  padding: 50px;
+  margin-left: 345px;
+  min-width: 1474px;
 `;
+
+export const Notification = (props) => {
+  const ImageWrapper = styled.div`
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    background-color: #ffedec;
+    border-radius: 8px;
+    transition: all 250ms ease-out;
+    margin-right: 22px;
+  `;
+
+  const NotificationIcon = styled(DefaultIcon)`
+    color: #e23428;
+  `;
+
+  const NotificationOuter = styled.div`
+    width: 340px;
+    background: #ffffff;
+    box-shadow: 0px 4px 4px #00000005;
+    border-radius: 12px;
+    padding: 30px;
+    display: inline-block;
+    margin-right: 20px;
+    transition: all 250ms ease-out;
+    cursor: pointer;
+
+    &:hover{
+      transform: scale(1.05);
+    }
+
+    &:hover ${ImageWrapper} {
+      background-color: #e23428;
+    }
+
+    &:hover ${NotificationIcon} {
+      color: #fff;
+    }
+  `;
+
+  const ContentWrapper = styled.div`
+    display: inline-block;
+  `;
+
+  const Icon = () => <NotificationIcon as={props.icon} />;
+
+  const NotificationNumber = styled.h5`
+    text-align: left;
+    font: normal normal 600 30px/46px Poppins;
+    letter-spacing: 0px;
+    color: #393939;
+  `;
+
+  const NotificationType = styled.p`
+    text-align: left;
+    font: normal normal 300 14px/21px Poppins;
+    letter-spacing: 0px;
+    color: #787878;
+  `;
+
+  const NotificationWrapper = styled.div`
+    display: flex;
+  `;
+
+  return (
+    <>
+      <NotificationOuter>
+        <NotificationWrapper>
+          <ImageWrapper>
+            <Icon />
+          </ImageWrapper>
+          <ContentWrapper>
+            <NotificationNumber>{props.number}</NotificationNumber>
+            <NotificationType>{props.text}</NotificationType>
+          </ContentWrapper>
+        </NotificationWrapper>
+      </NotificationOuter>
+    </>
+  );
+};
