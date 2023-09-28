7d37bb5c8d04d93f16863ff550d2c854502dad31
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-09-28 14:39:24 +0200

    Se añade array de datos ficticios para mapear en bookings

---
+++ src/data/bookingsjson.js
@@ -0,0 +1,146 @@
+export const bookingsData = [
+  {
+    guest: {
+      nombre: "Alice",
+      apellidos: "Johnson",
+      id_reserva: "HOTEL54321",
+    },
+    order_date: "2023-09-26",
+    check_in: "2023-10-10",
+    check_out: "2023-10-15",
+    special_request: "Early check-in required",
+    room: {
+      room_type: "Standard Room",
+      room_number: "101",
+    },
+    status: "Check In (verde)",
+  },
+  {
+    guest: {
+      nombre: "Bob",
+      apellidos: "Smith",
+      id_reserva: "HOTEL98765",
+    },
+    order_date: "2023-09-27",
+    check_in: "2023-11-05",
+    check_out: "2023-11-10",
+    special_request: "Allergic to pet dander",
+    room: {
+      room_type: "Suite",
+      room_number: "205",
+    },
+    status: "Check Out (rojo)",
+  },
+  {
+    guest: {
+      nombre: "Eva",
+      apellidos: "Brown",
+      id_reserva: "HOTEL24680",
+    },
+    order_date: "2023-09-28",
+    check_in: "2023-12-20",
+    check_out: "2023-12-25",
+    special_request: "Late check-out requested",
+    room: {
+      room_type: "Deluxe Suite",
+      room_number: "401",
+    },
+    status: "In Progress (amarillo)",
+  },
+  {
+    guest: {
+      nombre: "David",
+      apellidos: "Wilson",
+      id_reserva: "HOTEL13579",
+    },
+    order_date: "2023-09-29",
+    check_in: "2023-11-15",
+    check_out: "2023-11-20",
+    special_request: "Extra pillows and blankets",
+    room: {
+      room_type: "Standard Room",
+      room_number: "112",
+    },
+    status: "Check In (verde)",
+  },
+  {
+    guest: {
+      nombre: "Grace",
+      apellidos: "Johnson",
+      id_reserva: "HOTEL11223",
+    },
+    order_date: "2023-09-30",
+    check_in: "2023-10-08",
+    check_out: "2023-10-13",
+    special_request: "Room with a view of the garden",
+    room: {
+      room_type: "Junior Suite",
+      room_number: "301",
+    },
+    status: "In Progress (amarillo)",
+  },
+  {
+    guest: {
+      nombre: "Michael",
+      apellidos: "Davis",
+      id_reserva: "HOTEL56789",
+    },
+    order_date: "2023-10-01",
+    check_in: "2023-11-25",
+    check_out: "2023-11-30",
+    special_request: "No seafood in meals",
+    room: {
+      room_type: "Standard Room",
+      room_number: "205",
+    },
+    status: "Check In (verde)",
+  },
+  {
+    guest: {
+      nombre: "Sara",
+      apellidos: "Johnson",
+      id_reserva: "HOTEL54321",
+    },
+    order_date: "2023-10-02",
+    check_in: "2023-12-05",
+    check_out: "2023-12-10",
+    special_request: "Early check-in required",
+    room: {
+      room_type: "Deluxe Suite",
+      room_number: "501",
+    },
+    status: "Check Out (rojo)",
+  },
+  {
+    guest: {
+      nombre: "Fernanda",
+      apellidos: "Gómez",
+      id_reserva: "HOTEL24681",
+    },
+    order_date: "2023-09-30",
+    check_in: "2023-10-05",
+    check_out: "2023-10-10",
+    special_request: "Late check-in required",
+    room: {
+      room_type: "Standard Room",
+      room_number: "102",
+    },
+    status: "Check Out (rojo)",
+  },
+  {
+    guest: {
+      nombre: "Carlos",
+      apellidos: "López",
+      id_reserva: "HOTEL13580",
+    },
+    order_date: "2023-10-01",
+    check_in: "2023-11-01",
+    check_out: "2023-11-06",
+    special_request: "Non-smoking room",
+    room: {
+      room_type: "Suite",
+      room_number: "206",
+    },
+    status: "In Progress (amarillo)",
+  },
+];
