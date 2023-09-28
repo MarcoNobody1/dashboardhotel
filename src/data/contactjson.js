d9e0bfef2ead11add8d55e606e84dc22c86503bf
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-09-28 09:34:38 +0200

    Se añade array de datos ficticios para mapear en el dashboard

---
+++ src/data/contactjson.js
@@ -0,0 +1,29 @@
+export const contactMessages = [
+  {
+    id: "12345",
+    name: "John Doe",
+    email: "johndoe@example.com",
+    phone: "123-456-7890",
+    email_subject: "Hello",
+    email_description:
+      "This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.",
+  },
+  {
+    id: "67890",
+    name: "Jane Smith",
+    email: "janesmith@example.com",
+    phone: "987-654-3210",
+    email_subject: "Meeting Request",
+    email_description:
+      "I would like to request a meeting with you to discuss the project details.",
+  },
+  {
+    id: "54321",
+    name: "Robert Johnson",
+    email: "robertjohnson@example.com",
+    phone: "456-789-0123",
+    email_subject: "Important Announcement",
+    email_description:
+      "We have an important announcement regarding the upcoming event.",
+  },
+];
