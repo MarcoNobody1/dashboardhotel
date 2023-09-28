7b8846051b57c5256347b280560ddd023c939bec
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-09-28 09:36:03 +0200

    Se añaden los mensajes al dashboard y se termina styling del dashboard

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Notification, PageWrapper } from "../GeneralComponents";
+import { Comments, Notification, PageWrapper } from "../GeneralComponents";
 import { LiaBedSolid } from "react-icons/lia";
 import { LuCalendarCheck2 } from "react-icons/lu";
 import { BsBoxArrowInRight, BsBoxArrowRight } from "react-icons/bs";
@@ -9,6 +9,26 @@ const NotifContainer = styled.div`
   display: flex;
   width: 1474px;
   margin-bottom: 40px;
+
+`;
+
+const CommentsContainer = styled(NotifContainer)`
+  background-color: #fff;
+  margin-bottom: 0;
+  flex-direction: column;
+  padding: 30px;
+  width: 1474px;
+  box-shadow: 0px 4px 4px #00000005;
+  border-radius: 20px;
+  overflow: hidden;
+`;
+
+const CommentsTitle = styled.p`
+  text-align: left;
+  font: normal normal 500 20px/30px Poppins;
+  letter-spacing: 0px;
+  color: #393939;
+  margin-bottom: 30px;
 `;
 
 export const Dashboard = () => {
@@ -25,8 +45,10 @@ export const Dashboard = () => {
           <Notification number="753" text="Check In" icon={BsBoxArrowInRight} />
           <Notification number="516" text="Check Out" icon={BsBoxArrowRight} />
         </NotifContainer>
-
-
+        <CommentsContainer>
+          <CommentsTitle>Latest Review by Customers</CommentsTitle>
+          <Comments />
+        </CommentsContainer>
       </PageWrapper>
     </>
   );
