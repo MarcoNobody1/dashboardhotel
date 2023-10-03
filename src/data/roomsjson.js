df58ebabc932de4e3a29c989a076762d7060e955
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-03 08:04:03 +0200

    Se actualizan datos del json de rooms

---
+++ src/data/roomsjson.js
@@ -0,0 +1,582 @@
+export const RoomsData = [
+  {
+    "room_name": {
+      "id": "1ABCD123",
+      "room_photo": "https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
+      "room_number": 1234
+    },
+    "room_type": "Double Superior",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Nice Views",
+    ],
+    "price": 250,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 10
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "2EFGH456",
+      "room_photo": "https://ywcavan.org/sites/default/files/styles/scale_width_1440/public/assets/room/room_image/Single-bed-room-YWCA_Hotel_Vancouver.jpg",
+      "room_number": 5678
+    },
+    "room_type": "Single Bed",
+    "amenities": [
+      "1/3 Bed Space",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Coffee Set",
+    ],
+    "price": 120,
+    "offer_price": {
+      "isOffer": false,
+      "discount": 5
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "3IJKL789",
+      "room_photo": "https://galeriemagazine.com/wp-content/uploads/2019/03/243f89e0-8235-11e7-a767-bc310e55dd10_1320x770_154749-1024x597.jpg",
+      "room_number": 9876
+    },
+    "room_type": "Suite",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Bathtub",
+      "Jacuzzi",
+      "Nice Views",
+    ],
+    "price": 900,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 15
+    },
+    "status": "booked",
+  },
+  {
+    "room_name": {
+      "id": "4MNOPQ101",
+      "room_photo": "https://image-tc.galaxy.tf/wijpeg-clozx94odvn436foy8vjci4wz/empire-hotel-superior-double-room_standard.jpg",
+      "room_number": 2468
+    },
+    "room_type": "Double Bed",
+    "amenities": [
+      "1/2 Bathroom",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+    ],
+    "price": 180,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 8
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "5RSTU111",
+      "room_photo": "https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
+      "room_number": 1357
+    },
+    "room_type": "Double Superior",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Nice Views",
+    ],
+    "price": 275,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 12
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "6VWXYZ222",
+      "room_photo": "https://image-tc.galaxy.tf/wijpeg-clozx94odvn436foy8vjci4wz/empire-hotel-superior-double-room_standard.jpg",
+      "room_number": 3691
+    },
+    "room_type": "Double Bed",
+    "amenities": [
+      "1/2 Bathroom",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+    ],
+    "price": 200,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 10
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "7ABCDEF333",
+      "room_photo": "https://galeriemagazine.com/wp-content/uploads/2019/03/243f89e0-8235-11e7-a767-bc310e55dd10_1320x770_154749-1024x597.jpg",
+      "room_number": 7777
+    },
+    "room_type": "Suite",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Bathtub",
+      "Jacuzzi",
+      "Nice Views",
+    ],
+    "price": 950,
+    "offer_price": {
+      "isOffer": false,
+      "discount": 0
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "8GHIJK444",
+      "room_photo": "https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
+      "room_number": 8888
+    },
+    "room_type": "Double Superior",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Nice Views",
+    ],
+    "price": 300,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 18
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "9LMNOP555",
+      "room_photo": "https://ywcavan.org/sites/default/files/styles/scale_width_1440/public/assets/room/room_image/Single-bed-room-YWCA_Hotel_Vancouver.jpg",
+      "room_number": 9999
+    },
+    "room_type": "Single Bed",
+    "amenities": [
+      "1/3 Bed Space",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Coffee Set",
+    ],
+    "price": 130,
+    "offer_price": {
+      "isOffer": false,
+      "discount": 0
+    },
+    "status": "booked",
+  },
+  {
+    "room_name": {
+      "id": "10QRSTU666",
+      "room_photo": "https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
+      "room_number": 4321
+    },
+    "room_type": "Double Superior",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Nice Views",
+    ],
+    "price": 280,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 10
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "11VWXYZ777",
+      "room_photo": "https://image-tc.galaxy.tf/wijpeg-clozx94odvn436foy8vjci4wz/empire-hotel-superior-double-room_standard.jpg",
+      "room_number": 5678
+    },
+    "room_type": "Double Bed",
+    "amenities": [
+      "1/2 Bathroom",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+    ],
+    "price": 190,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 5
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "12XYZ123",
+      "room_photo": "https://galeriemagazine.com/wp-content/uploads/2019/03/243f89e0-8235-11e7-a767-bc310e55dd10_1320x770_154749-1024x597.jpg",
+      "room_number": 9876
+    },
+    "room_type": "Suite",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Bathtub",
+      "Jacuzzi",
+      "Nice Views",
+    ],
+    "price": 970,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 20
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "13ABC456",
+      "room_photo": "https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
+      "room_number": 1111
+    },
+    "room_type": "Double Superior",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Nice Views",
+    ],
+    "price": 270,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 12
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "14DEFGH789",
+      "room_photo": "https://ywcavan.org/sites/default/files/styles/scale_width_1440/public/assets/room/room_image/Single-bed-room-YWCA_Hotel_Vancouver.jpg",
+      "room_number": 2222
+    },
+    "room_type": "Single Bed",
+    "amenities": [
+      "1/3 Bed Space",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Coffee Set",
+    ],
+    "price": 125,
+    "offer_price": {
+      "isOffer": false,
+      "discount": 8
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "15IJKLM101",
+      "room_photo": "https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
+      "room_number": 3333
+    },
+    "room_type": "Double Superior",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Nice Views",
+    ],
+    "price": 290,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 15
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "16NOPQR111",
+      "room_photo": "https://image-tc.galaxy.tf/wijpeg-clozx94odvn436foy8vjci4wz/empire-hotel-superior-double-room_standard.jpg",
+      "room_number": 4444
+    },
+    "room_type": "Double Bed",
+    "amenities": [
+      "1/2 Bathroom",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+    ],
+    "price": 175,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 10
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "17STUV123",
+      "room_photo": "https://galeriemagazine.com/wp-content/uploads/2019/03/243f89e0-8235-11e7-a767-bc310e55dd10_1320x770_154749-1024x597.jpg",
+      "room_number": 5555
+    },
+    "room_type": "Suite",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Bathtub",
+      "Jacuzzi",
+      "Nice Views",
+    ],
+    "price": 920,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 8
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "18UVWX456",
+      "room_photo": "https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
+      "room_number": 6666
+    },
+    "room_type": "Double Superior",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Nice Views",
+    ],
+    "price": 310,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 18
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "19WXYZ789",
+      "room_photo": "https://ywcavan.org/sites/default/files/styles/scale_width_1440/public/assets/room/room_image/Single-bed-room-YWCA_Hotel_Vancouver.jpg",
+      "room_number": 7777
+    },
+    "room_type": "Single Bed",
+    "amenities": [
+      "1/3 Bed Space",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Coffee Set",
+    ],
+    "price": 135,
+    "offer_price": {
+      "isOffer": false,
+      "discount": 0
+    },
+    "status": "booked",
+  },
+  {
+    "room_name": {
+      "id": "20XYZABC101",
+      "room_photo": "https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
+      "room_number": 8888
+    },
+    "room_type": "Double Superior",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Nice Views",
+    ],
+    "price": 265,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 10
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "21ABCDEF111",
+      "room_photo": "https://image-tc.galaxy.tf/wijpeg-clozx94odvn436foy8vjci4wz/empire-hotel-superior-double-room_standard.jpg",
+      "room_number": 9999
+    },
+    "room_type": "Double Bed",
+    "amenities": [
+      "1/2 Bathroom",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+    ],
+    "price": 210,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 5
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "22DEFGHI222",
+      "room_photo": "https://galeriemagazine.com/wp-content/uploads/2019/03/243f89e0-8235-11e7-a767-bc310e55dd10_1320x770_154749-1024x597.jpg",
+      "room_number": 1234
+    },
+    "room_type": "Suite",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Bathtub",
+      "Jacuzzi",
+      "Nice Views",
+    ],
+    "price": 980,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 15
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "23GHIJKL333",
+      "room_photo": "https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
+      "room_number": 5678
+    },
+    "room_type": "Double Superior",
+    "amenities": [
+      "1/3 Bed Space",
+      "24-Hour Guard",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Mini Bar",
+      "Coffee Set",
+      "Nice Views",
+    ],
+    "price": 290,
+    "offer_price": {
+      "isOffer": true,
+      "discount": 12
+    },
+    "status": "available",
+  },
+  {
+    "room_name": {
+      "id": "24JKLMNO444",
+      "room_photo": "https://ywcavan.org/sites/default/files/styles/scale_width_1440/public/assets/room/room_image/Single-bed-room-YWCA_Hotel_Vancouver.jpg",
+      "room_number": 9876
+    },
+    "room_type": "Single Bed",
+    "amenities": [
+      "1/3 Bed Space",
+      "Free Wifi",
+      "Air Conditioner",
+      "Television",
+      "Towels",
+      "Coffee Set",
+    ],
+    "price": 140,
+    "offer_price": {
+      "isOffer": false,
+      "discount": 0
+    },
+    "status": "booked",
+  },
+];
