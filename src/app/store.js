93cef7522ffe534ac14635ea74d3c7875f6757b0
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-03 22:27:20 +0200

    Se implementa el slice de bookings en la store de redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { bookingSlice } from '../features/Bookings/bookingSlice';
 
 export const store = configureStore({
     reducer: {
+      bookings: bookingSlice.reducer,
     },
   });
