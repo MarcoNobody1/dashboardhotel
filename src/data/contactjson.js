558d76715915d65314f1acb4a883313924a4edf7
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-03 10:22:49 +0200

    Se añaden  datos a contactjson

--- src/data/contactjson.js
+++ src/data/contactjson.js
@@ -1,29 +1,35 @@
 export const contactMessages = [
   {
     id: "12345",
+    date:"2023-11-05",
     name: "John Doe",
     email: "johndoe@example.com",
     phone: "123-456-7890",
     email_subject: "Hello",
     email_description:
       "This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.",
+    archived: false,
   },
   {
     id: "67890",
+    date:"2023-12-14",
     name: "Jane Smith",
     email: "janesmith@example.com",
     phone: "987-654-3210",
     email_subject: "Meeting Request",
     email_description:
       "I would like to request a meeting with you to discuss the project details.",
+    archived: false,
   },
   {
     id: "54321",
+    date:"2022-05-25",
     name: "Robert Johnson",
     email: "robertjohnson@example.com",
     phone: "456-789-0123",
     email_subject: "Important Announcement",
     email_description:
       "We have an important announcement regarding the upcoming event.",
+    archived: false,
   },
 ];
