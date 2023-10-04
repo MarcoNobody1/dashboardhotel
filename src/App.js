bc3c5b5dd709a1d5aeb7126767aaced5c93c4229
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-04 19:37:49 +0200

    Se implementa authcontainer y private routes en app para condicionar los redireccionamientos y la logica general de la pagina

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./Login/Login";
 import { Dashboard } from "./Dashboard/Dashboard";
@@ -10,78 +9,29 @@ import { Users } from "./Users/Users";
 import { Header } from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
 import BookingDetails from "./Bookings/BookingDetails";
-import { useDispatch } from "react-redux";
-import { getData } from "./features/Bookings/bookingThunks";
+import { AuthContainer } from "./Login/Auth";
 
 export const App = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [title, setTitle] = useState("Dashboard")
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const isLoggedIn = localStorage.getItem("log");
-    if (isLoggedIn) {
-      setLoggedIn(true);
-    }
-      dispatch(getData());
-  }, [dispatch]);
-
   return (
     <>
       <BrowserRouter>
-        {loggedIn && <Sidebar setTitle={setTitle}/>}
-        {loggedIn && <Header titleText={title} setLoggedIn={setLoggedIn} />}
-        <Routes>
-          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} loggedIn={loggedIn}/>} />
-          <Route
-            path="/"
-            element={
-              <PrivateRoute loggedIn={loggedIn}>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/bookings"
-            element={
-              <PrivateRoute loggedIn={loggedIn}>
-                <Bookings />
-              </PrivateRoute>
-            }
-          />
-           <Route
-            path="/bookings/:id"
-            element={
-              <PrivateRoute loggedIn={loggedIn}>
-                <BookingDetails />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/rooms"
-            element={
-              <PrivateRoute loggedIn={loggedIn}>
-                <Rooms />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/contact"
-            element={
-              <PrivateRoute loggedIn={loggedIn}>
-                {" "}
-                <Contact />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/users"
-            element={
-              <PrivateRoute loggedIn={loggedIn}>
-                <Users />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
+        <AuthContainer>
+          <Sidebar />
+          <Header />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          <PrivateRoute>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/bookings" element={<Bookings />} />
+              <Route path="/bookings/:id" element={<BookingDetails />} />
+              <Route path="/rooms" element={<Rooms />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/users" element={<Users />} />
+            </Routes>
+          </PrivateRoute>
+        </AuthContainer>
       </BrowserRouter>
     </>
   );
