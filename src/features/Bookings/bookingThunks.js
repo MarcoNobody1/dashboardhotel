041330eabe1922d5697cb492f04384b5a44b2d4f
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-04 19:36:49 +0200

    Se modifica logica de llamada al thunk de bookings para que cumpla con su asincronia

--- src/features/Bookings/bookingThunks.js
+++ src/features/Bookings/bookingThunks.js
@@ -1,6 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { bookingsData } from "../../data/bookingsjson";
 
+const delay = (data, time = 500) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(data);
+    }, time);
+  });
+};
+
 export const getData = createAsyncThunk("bookings/getData", async () => {
-  return bookingsData;
+  return await delay(bookingsData);
 });
