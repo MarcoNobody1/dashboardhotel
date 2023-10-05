cd8118700819c5d68b68c8596f1c588b4d647ccb
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-05 13:45:13 +0200

    Se crean thunks para los casos para borrar data y solicitar un solo objeto en el array de bookings

--- src/features/Bookings/bookingThunks.js
+++ src/features/Bookings/bookingThunks.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { bookingsData } from "../../data/bookingsjson";
 
-const delay = (data, time = 500) => {
+const delay = (data, time = 400) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(data);
@@ -11,4 +11,12 @@ const delay = (data, time = 500) => {
 
 export const getData = createAsyncThunk("bookings/getData", async () => {
   return await delay(bookingsData);
+});
+
+export const deleteData = createAsyncThunk("bookings/deleteData", async (id) => {
+  return await delay(id);
+});
+
+export const get1Data = createAsyncThunk("bookings/get1Data", async (id) => {
+  return await delay(id);
 });
