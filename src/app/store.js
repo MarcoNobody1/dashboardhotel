f6acabba8cebdfaef026b61f43257d9c13fa79b2
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-05 23:36:34 +0200

    Se añade reducer de rooms al store de redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { bookingSlice } from '../features/Bookings/bookingSlice';
+import { roomSlice } from '../features/Rooms/roomSlice';
 
 export const store = configureStore({
     reducer: {
       bookings: bookingSlice.reducer,
+      rooms: roomSlice.reducer,
     },
   });
