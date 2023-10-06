f25aa3ea3270955f9e039a773e8f6426a96a17ec
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-06 07:10:03 +0200

    Se implementa el slice de contacts en el store de redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { bookingSlice } from '../features/Bookings/bookingSlice';
 import { roomSlice } from '../features/Rooms/roomSlice';
+import { contactSlice } from '../features/Contact/contactSlice';
 
 export const store = configureStore({
     reducer: {
       bookings: bookingSlice.reducer,
       rooms: roomSlice.reducer,
+      contacts: contactSlice.reducer,
     },
   });
