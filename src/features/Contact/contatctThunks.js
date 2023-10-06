12eec83ee3d818defd909bfe9830a122a7e8f37b
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-06 07:09:44 +0200

    Se crean thunks para contacts

---
+++ src/features/Contact/contatctThunks.js
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { contactMessages } from "../../data/contactjson";
+
+const delay = (data, time = 400) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(data);
+    }, time);
+  });
+};
+
+export const getContactsData = createAsyncThunk("contacts/getContactData", async () => {
+
+  return await delay(contactMessages);
+});
+
+export const deleteContactsData = createAsyncThunk("contacts/deleteContactsData", async (id) => {
+  return await delay(id);
+});
+
+export const get1ContactData = createAsyncThunk("contacts/get1ContactData", async (id) => {
+  return await delay(id);
+});
