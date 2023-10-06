700bd0a733ea4de1d7dcce361c01166ae91d1154
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-06 09:30:31 +0200

    Se implementa reducer de users en el store de redux

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { bookingSlice } from '../features/Bookings/bookingSlice';
 import { roomSlice } from '../features/Rooms/roomSlice';
 import { contactSlice } from '../features/Contact/contactSlice';
+import { userSlice } from '../features/Users/userSlice';
 
 export const store = configureStore({
     reducer: {
       bookings: bookingSlice.reducer,
       rooms: roomSlice.reducer,
       contacts: contactSlice.reducer,
+      users: userSlice.reducer,
     },
   });
