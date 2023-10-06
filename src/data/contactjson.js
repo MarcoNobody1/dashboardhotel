1161c0b522b5f301bf2994340b0afde5b9d54e62
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-06 07:09:13 +0200

    Se modifica json de datos de contacts

--- src/data/contactjson.js
+++ src/data/contactjson.js
@@ -1,35 +1,44 @@
 export const contactMessages = [
   {
-    id: "12345",
-    date:"2023-11-05",
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "123-456-7890",
-    email_subject: "Hello",
-    email_description:
-      "This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.",
-    archived: false,
+    "date": {
+      "id": "12345",
+      "send_date": "2023-11-05"
+    },
+    "customer": {
+      "name": "John Doe",
+      "email": "johndoe@example.com",
+      "phone": "123-456-7890"
+    },
+    "subject": "Hello",
+    "comment": "This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.This is a sample email description.",
+    "archived": false
   },
   {
-    id: "67890",
-    date:"2023-12-14",
-    name: "Jane Smith",
-    email: "janesmith@example.com",
-    phone: "987-654-3210",
-    email_subject: "Meeting Request",
-    email_description:
-      "I would like to request a meeting with you to discuss the project details.",
-    archived: false,
+    "date": {
+      "id": "67890",
+      "send_date": "2023-12-14"
+    },
+    "customer": {
+      "name": "Jane Smith",
+      "email": "janesmith@example.com",
+      "phone": "987-654-3210"
+    },
+    "subject": "Meeting Request",
+    "comment": "I would like to request a meeting with you to discuss the project details.",
+    "archived": false
   },
   {
-    id: "54321",
-    date:"2022-05-25",
-    name: "Robert Johnson",
-    email: "robertjohnson@example.com",
-    phone: "456-789-0123",
-    email_subject: "Important Announcement",
-    email_description:
-      "We have an important announcement regarding the upcoming event.",
-    archived: false,
-  },
+    "date": {
+      "id": "54321",
+      "send_date": "2022-05-25"
+    },
+    "customer": {
+      "name": "Robert Johnson",
+      "email": "robertjohnson@example.com",
+      "phone": "456-789-0123"
+    },
+    "subject": "Important Announcement",
+    "comment": "We have an important announcement regarding the upcoming event.",
+    "archived": false
+  }
 ];
