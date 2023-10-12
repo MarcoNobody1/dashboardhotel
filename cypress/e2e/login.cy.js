f942e4881c07cb71dc4482c40c80ea63218f77cd
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-12 12:50:43 +0200

    se cambia descripcion de los tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,5 @@
-describe("Pruebas de logeo en mi aplicacion", () => {
-  it("Si meto los datos correctos me lleva a la pagina principal", () => {
+describe("Pruebas de logeo en la aplicacion", () => {
+  it("Si se introducen los datos correctos me lleva a la pagina principal", () => {
     cy.visit("http://localhost:3000/login");
     cy.get('[data-cy="usernameInput"]').type("marcocamaradiaz@gmail.com");
     cy.get('[data-cy="passwordInput"]').type("Marco");
@@ -7,7 +7,7 @@ describe("Pruebas de logeo en mi aplicacion", () => {
     cy.location("pathname").should("not.include", "/login");
   });
 
-  it("Si meto los datos incorrectos me salta un modal de error y no me deja entrar a la pagina principal", () => {
+  it("Si se introducen los datos incorrectos me salta un modal de error y no me deja entrar a la pagina principal", () => {
     cy.visit("http://localhost:3000/login");
     cy.get('[data-cy="usernameInput"]').type("hola@gmail.com");
     cy.get('[data-cy="passwordInput"]').type("mundo");
@@ -16,14 +16,14 @@ describe("Pruebas de logeo en mi aplicacion", () => {
     cy.contains("em", "Please check hint before logging.");
   });
 
-  it("Si no me he logeado, no puedo navegar a ninguna otra pagina", () => {
+  it("Si no se ha realizado el logeo, no se puede navegar a ninguna otra pagina", () => {
     cy.visit("http://localhost:3000/");
     cy.location("pathname").should("include", "login");
     cy.visit("http://localhost:3000/bookings");
     cy.location("pathname").should("include", "login");
   });
 
-  it("Una vez logeado, no puedo volver a /login", () => {
+  it("Una vez logeado, no se puede volver a /login", () => {
     cy.visit("http://localhost:3000/login");
     cy.get('[data-cy="usernameInput"]').type("marcocamaradiaz@gmail.com");
     cy.get('[data-cy="passwordInput"]').type("Marco");
