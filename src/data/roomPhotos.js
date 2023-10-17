b3fea053ccec9746808c35a3c1c1024331616bde
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-17 10:01:58 +0200

    Se aisla la data de las fotos para las nuevas rooms

---
+++ src/data/roomPhotos.js
@@ -0,0 +1,7 @@
+export const roomPhotos = [
+    "https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
+    "https://ywcavan.org/sites/default/files/styles/scale_width_1440/public/assets/room/room_image/Single-bed-room-YWCA_Hotel_Vancouver.jpg",
+    "https://galeriemagazine.com/wp-content/uploads/2019/03/243f89e0-8235-11e7-a767-bc310e55dd10_1320x770_154749-1024x597.jpg",
+    "https://image-tc.galaxy.tf/wijpeg-clozx94odvn436foy8vjci4wz/empire-hotel-superior-double-room_standard.jpg",
+    "https://www.cvent.com/sites/default/files/image/2021-10/hotel%20room%20with%20beachfront%20view.jpg",
+  ];
