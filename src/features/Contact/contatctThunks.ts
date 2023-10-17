112c078cd967599cfddd765292571497a39b4604
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-18 01:37:52 +0200

    Se migra contactThunks a typescript

---
+++ src/features/Contact/contatctThunks.ts
@@ -0,0 +1,24 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { contactMessages } from "../../data/contactjson";
+import { ContactInterface } from "../Interfaces/Interfaces";
+
+const delay = (data:ContactInterface[] | string | ContactInterface, time:number = 400) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(data);
+    }, time);
+  });
+};
+
+export const getContactsData = createAsyncThunk<ContactInterface[]>("contacts/getContactData", async () => {
+
+  return (await delay(contactMessages)) as ContactInterface[];
+});
+
+export const deleteContactsData = createAsyncThunk("contacts/deleteContactsData", async (id:string) => {
+  return (await delay(id)) as string;
+});
+
+export const get1ContactData = createAsyncThunk("contacts/get1ContactData", async (id:string) => {
+  return (await delay(id)) as string;
+});
