73cf14b47c590194c9c97503d300f6337446314d
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-18 01:38:24 +0200

    Se corrigen errores de tipado de bookingThunks

--- src/features/Bookings/bookingThunks.ts
+++ src/features/Bookings/bookingThunks.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { bookingsData } from "../../data/bookingsjson";
 import { BookingInterface } from "../Interfaces/Interfaces";
 
-const delay = (data:object[] | string, time:number = 400) => {
+const delay = (data:BookingInterface[] | string | BookingInterface, time:number = 400) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(data);
@@ -15,9 +15,9 @@ export const getData = createAsyncThunk<BookingInterface[]>("bookings/getData", 
 });
 
 export const deleteData = createAsyncThunk("bookings/deleteData", async (id:string) => {
-  return await delay(id);
+  return (await delay(id)) as string;
 });
 
 export const get1Data = createAsyncThunk("bookings/get1Data", async (id:string) => {
-  return await delay(id);
+  return (await delay(id)) as string;
 });
