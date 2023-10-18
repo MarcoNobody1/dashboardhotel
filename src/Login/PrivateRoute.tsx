a54c4f68f0edf9be09d887f65a8a3d2452387352
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-18 21:23:17 +0200

    Se migra PrivateRoute a TS

---
+++ src/Login/PrivateRoute.tsx
@@ -0,0 +1,21 @@
+import { FC, ReactNode, useContext } from "react";
+import { Navigate } from "react-router";
+import { AuthContext } from "./Auth";
+
+interface PrivateRouteInterface {
+  children: ReactNode
+}
+
+export const PrivateRoute: FC<PrivateRouteInterface> = ({ children }) => {
+const authContext = useContext(AuthContext);
+
+  if (!authContext || !authContext.auth.authenticated) {
+    return (
+      <>
+        <Navigate to="/login" />
+      </>
+    );
+  }
+
+  return children
+};
