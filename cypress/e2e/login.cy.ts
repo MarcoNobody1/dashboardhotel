4fe3b5ffb605e3a2e429ca8b5a872d805801ff82
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-20 23:12:11 +0200

    Se adaptan pruebas de cypress a TS

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -2,7 +2,7 @@ describe("Pruebas de logeo en la aplicacion", () => {
 
   beforeEach(() => {
 
-    cy.visit("http://localhost:3000/login");
+    cy.visit("http://localhost:5173/login");
   });
 
   it("Si se introducen los datos correctos me lleva a la pagina principal", () => {
@@ -21,9 +21,9 @@ describe("Pruebas de logeo en la aplicacion", () => {
   });
 
   it("Si no se ha realizado el logeo, no se puede navegar a ninguna otra pagina", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("http://localhost:5173/");
     cy.location("pathname").should("include", "login");
-    cy.visit("http://localhost:3000/bookings");
+    cy.visit("http://localhost:5173/bookings");
     cy.location("pathname").should("include", "login");
   });
 
@@ -31,7 +31,7 @@ describe("Pruebas de logeo en la aplicacion", () => {
     cy.get('[data-cy="usernameInput"]').type("marcocamaradiaz@gmail.com");
     cy.get('[data-cy="passwordInput"]').type("Marco");
     cy.get('[data-cy="loginButton"]').click();
-    cy.visit("http://localhost:3000/login");
+    cy.visit("http://localhost:5173/login");
     cy.location("pathname").should("not.include", "login");
   });
 
@@ -39,7 +39,7 @@ describe("Pruebas de logeo en la aplicacion", () => {
     cy.get('[data-cy="usernameInput"]').type("marcocamaradiaz@gmail.com");
     cy.get('[data-cy="passwordInput"]').type("Marco");
     cy.get('[data-cy="loginButton"]').click();
-    cy.visit("http://localhost:3000/rooms");
+    cy.visit("http://localhost:5173/rooms");
     cy.location("pathname").should("include", "rooms");
   });
 });
