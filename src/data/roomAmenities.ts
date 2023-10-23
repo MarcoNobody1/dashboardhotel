2b447c148452d184db86aca19acb949894f01f54
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-23 04:13:56 +0200

    Se exporta la interfaz de roomAmenities

--- src/data/roomAmenities.ts
+++ src/data/roomAmenities.ts
@@ -1,50 +1,49 @@
-interface roomAmenitiesInterface {
-  Standard:string[],
-  Advanced: string[],
-  Premium: string[],
-  FullRoom: string[],
+export interface roomAmenitiesInterface {
+  Standard: string[];
+  Advanced: string[];
+  Premium: string[];
+  FullRoom: string[];
 }
 
-
-export const roomAmenities:roomAmenitiesInterface = {
-    Standard: [
-      "1/3 Bed Space",
-      "Air Conditioner",
-      "Television",
-      "Towels",
-      "Coffee Set",
-    ],
-    Advanced: [
-      "1/2 Bathroom",
-      "Free Wifi",
-      "Air Conditioner",
-      "Television",
-      "Towels",
-      "Mini Bar",
-      "Coffee Set",
-    ],
-    Premium: [
-      "1/3 Bed Space",
-      "24-Hour Guard",
-      "Free Wifi",
-      "Air Conditioner",
-      "Television",
-      "Towels",
-      "Mini Bar",
-      "Coffee Set",
-      "Nice Views",
-    ],
-    FullRoom: [
-      "1/3 Bed Space",
-      "24-Hour Guard",
-      "Free Wifi",
-      "Air Conditioner",
-      "Television",
-      "Towels",
-      "Mini Bar",
-      "Coffee Set",
-      "Bathtub",
-      "Jacuzzi",
-      "Nice Views",
-    ],
-  };
+export const roomAmenities: roomAmenitiesInterface = {
+  Standard: [
+    "1/3 Bed Space",
+    "Air Conditioner",
+    "Television",
+    "Towels",
+    "Coffee Set",
+  ],
+  Advanced: [
+    "1/2 Bathroom",
+    "Free Wifi",
+    "Air Conditioner",
+    "Television",
+    "Towels",
+    "Mini Bar",
+    "Coffee Set",
+  ],
+  Premium: [
+    "1/3 Bed Space",
+    "24-Hour Guard",
+    "Free Wifi",
+    "Air Conditioner",
+    "Television",
+    "Towels",
+    "Mini Bar",
+    "Coffee Set",
+    "Nice Views",
+  ],
+  FullRoom: [
+    "1/3 Bed Space",
+    "24-Hour Guard",
+    "Free Wifi",
+    "Air Conditioner",
+    "Television",
+    "Towels",
+    "Mini Bar",
+    "Coffee Set",
+    "Bathtub",
+    "Jacuzzi",
+    "Nice Views",
+  ],
+};
