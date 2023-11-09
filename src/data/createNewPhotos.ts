a8c37ca6500c4054f3f3dc47dc8d1823ca5a325f
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-10 00:40:22 +0100

    Se modifican URLs de fotos

--- src/data/createNewPhotos.ts
+++ src/data/createNewPhotos.ts
@@ -1,9 +1,9 @@
 export const roomPhotos:string[] = [
-    "https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
-    "https://ywcavan.org/sites/default/files/styles/scale_width_1440/public/assets/room/room_image/Single-bed-room-YWCA_Hotel_Vancouver.jpg",
-    "https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg",
-    "https://image-tc.galaxy.tf/wijpeg-clozx94odvn436foy8vjci4wz/empire-hotel-superior-double-room_standard.jpg",
-    "https://www.cvent.com/sites/default/files/image/2021-10/hotel%20room%20with%20beachfront%20view.jpg",
+    "https://tinyurl.com/PhotoNumberOne",
+    "https://tinyurl.com/PhotoNumberTwo",
+    "https://tinyurl.com/PhotoNumberThreee",
+    "https://tinyurl.com/PhotoNumberFour",
+    "https://tinyurl.com/PhotoNumberFive",
   ];
 
 const randomnumber = Math.floor(Math.random() * 100) + 1;
