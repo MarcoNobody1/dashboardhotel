f1f8629252634d9383e82dbe7205e920ed884536
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-16 23:52:11 +0100

    se añade logica para realizar dispatch one si estas dentro de x/details y haces refresh

--- src/Components/GeneralComponents/RenderStatus.tsx
+++ src/Components/GeneralComponents/RenderStatus.tsx
@@ -1,14 +1,29 @@
 import { ReactNode } from "react";
 import { RenderError, RenderGlassLoading } from "./GeneralComponents";
+import { useLocation, useParams } from "react-router";
+import { useAppDispatch } from "../../app/hooks";
+import { get1RoomData } from "../../features/Rooms/roomThunks";
+import { get1Data } from "../../features/Bookings/bookingThunks";
 
-export const renderStatus = (status:string, data: () => ReactNode):ReactNode => {
-    if (status === "fulfilled") {
-      return (
-        data()
-      );
-    } else if (status === "rejected") {
-      return <RenderError/>;
-    } else {
-      return <RenderGlassLoading />;
+export const renderStatus = (
+  status: string,
+  data: () => ReactNode
+): ReactNode => {
+  const location = useLocation();
+  const dispatch = useAppDispatch();
+  const { id } = useParams();
+
+  if (status === "fulfilled") {
+    return data();
+  } else if (status === "rejected") {
+    return <RenderError />;
+  } else if (status === "pending") {
+    return <RenderGlassLoading />;
+  } else if (status === "idle") {
+    if (location.pathname.startsWith("/rooms/") && id) {
+      dispatch(get1RoomData(id));
+    } else if (location.pathname.startsWith("/bookings/") && id) {
+      dispatch(get1Data(id));
     }
-  };
+  }
+};
