0c0656200961a54c6e59f80162d772849e8d1030
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-16 23:44:45 +0100

    Se termina la logica del thunk hacia la URL de lambda

--- src/features/Users/userThunks.ts
+++ src/features/Users/userThunks.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { UserInterface } from "../Interfaces/Interfaces";
-import users from "../../data/MirandaDashboard.users.json";
-const delay = (data:UserInterface[] | string | UserInterface, time = 400) => {
+
+const delay = (data: UserInterface[] | string | UserInterface, time = 400) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(data);
@@ -15,15 +15,13 @@ const token = localStorage.getItem("token") || "";
 export const getUsersData = createAsyncThunk<UserInterface[]>(
   "users/getUsersData",
   async () => {
-   
-
     const response = await fetch(`${localUrl}users`, {
       mode: "cors",
       method: "GET",
       headers: {
         "Content-Type": "application/json",
         token: token,
-      }
+      },
     });
     if (!response.ok) throw new Error(`Status: ${response.status}`);
     const data = await response.json();
@@ -32,20 +30,73 @@ export const getUsersData = createAsyncThunk<UserInterface[]>(
   }
 );
 
-export const deleteUsersData = createAsyncThunk("users/deleteUsersData", async (id:string) => {
-  return (await delay(id)) as string;
-});
+export const deleteUsersData = createAsyncThunk(
+  "users/deleteUsersData",
+  async (id: string) => {
+    const response = await fetch(`${localUrl}users/${id}`, {
+      mode: "cors",
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+    });
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
 
-export const get1UserData = createAsyncThunk("users/get1UserData", async (id:string) => {
-  return (await delay(id)) as string;
-});
+    return id as string;
+  }
+);
 
-export const addUserData = createAsyncThunk("rooms/addUserData", async (user:UserInterface) => {
-  
-  return( await delay(user)) as UserInterface;
-})
+export const get1UserData = createAsyncThunk(
+  "users/get1UserData",
+  async (id: string) => {
+    const response = await fetch(`${localUrl}users/${id}`, {
+      mode: "cors",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+    });
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
+    const data = await response.json();
 
-export const updateUserData = createAsyncThunk("rooms/updateUserData", async (user:UserInterface) => {
-  
-  return( await delay(user)) as UserInterface;
-})
+    return data as UserInterface;
+  }
+);
+
+export const addUserData = createAsyncThunk(
+  "rooms/addUserData",
+  async (user: Partial<UserInterface>) => {
+    const response = await fetch(`${localUrl}users`, {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+      body: JSON.stringify(user),
+    });
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
+
+    return user;
+  }
+);
+
+export const updateUserData = createAsyncThunk(
+  "rooms/updateUserData",
+  async (user: Partial<UserInterface>) => {
+    const response = await fetch(`${localUrl}users/${user._id}`, {
+      mode: "cors",
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+      body: JSON.stringify(user),
+    });
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
+
+    return user;
+  }
+);
