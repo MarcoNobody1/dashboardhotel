7fddedeadf810d01a526b8203155248002ecdb82
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-16 23:45:01 +0100

    Se termina la logica del thunk hacia la URL de lambda

--- src/features/Contact/contactThunks.ts
+++ src/features/Contact/contactThunks.ts
@@ -1,27 +1,77 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { ContactInterface } from "../Interfaces/Interfaces";
-import contacts from "../../data/MirandaDashboard.contacts.json";
 
-const delay = (data:ContactInterface[] | string | ContactInterface, time:number = 400) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(data);
-    }, time);
-  });
-};
+const localUrl = import.meta.env.VITE_FETCH_URL;
+const token = localStorage.getItem("token") || "";
 
-export const getContactsData = createAsyncThunk<ContactInterface[]>("contacts/getContactData", async () => {
+export const getContactsData = createAsyncThunk<ContactInterface[]>(
+  "contacts/getContactData",
+  async () => {
+    const response = await fetch(`${localUrl}contacts`, {
+      mode: "cors",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+    });
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
+    const data = await response.json();
 
-  return (await delay(contacts)) as ContactInterface[];
-});
+    return data as ContactInterface[];
+  }
+);
 
-export const deleteContactsData = createAsyncThunk("contacts/deleteContactsData", async (id:string) => {
-  return (await delay(id)) as string;
-});
+export const deleteContactsData = createAsyncThunk(
+  "contacts/deleteContactsData",
+  async (id: string) => {
+    const response = await fetch(`${localUrl}contacts/${id}`, {
+      mode: "cors",
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+    });
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
 
-export const get1ContactData = createAsyncThunk("contacts/get1ContactData", async (id:string) => {
-  return (await delay(id)) as string;
-});
-export const archiveData = createAsyncThunk("contacts/archiveData", async (id:string) => {
-  return await delay(id);
-});
+    return id as string;
+  }
+);
+
+export const get1ContactData = createAsyncThunk(
+  "contacts/get1ContactData",
+  async (id: string) => {
+    const response = await fetch(`${localUrl}contacts/${id}`, {
+      mode: "cors",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+    });
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
+    const data = await response.json();
+
+    return data as ContactInterface;
+  }
+);
+export const archiveData = createAsyncThunk(
+  "contacts/archiveData",
+  async (update: ContactInterface) => {
+    const response = await fetch(`${localUrl}contacts/${update._id}`, {
+      mode: "cors",
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+      body: JSON.stringify({
+        archived: !update.archived
+      }),
+    });
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
+
+    return update._id as string;
+  }
+);
